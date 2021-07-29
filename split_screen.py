adcb315d80eb0a576f6716f600aa8b006a973fb1
slamon-love <ssakirus@gmail.com>
2021-07-29 22:15:12 +0900

    add button

--- split_screen.py
+++ split_screen.py
@@ -2,18 +2,29 @@ from tkinter import *
 
 root1 = Tk()
 
-btn1 = Button(root1,color='green')
-btn2 = Button(root1,color='blue')
-btn3 = Button(root1,color='yellow')
+btn1 = Button(root1,padx=10, pady=5, bg="green")
+btn2 = Button(root1,padx=10, pady=5, bg="blue")
+btn3 = Button(root1,padx=10, pady=5, bg="yellow")
+btn1.grid(row=0, column=1)
+btn2.grid(row=0, column=2)
+btn3.grid(row=0, column=3)
 
-root2 = Tk()
-root3 = Tk()
 
-root1.geometry("")
-root2.geometry("")
-root3.geometry("")
+
+# root2 = Tk()
+# root3 = Tk()
+# root4 = Tk()
+
+
+root1.geometry("120x30")
+# root2.geometry("")
+# root3.geometry("")
+# root4.geometry("")
+
 
 root1.mainloop()
-root2.mainloop()
-root3.mainloop()
+# root2.mainloop()
+# root3.mainloop()
+# root4.mainloop()
+
 
