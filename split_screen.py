8ff9d2a064e05f85910bf57ee5cde70a13f9a8b4
slamon-love <ssakirus@gmail.com>
2021-07-29 21:18:04 +0900

    1

---
+++ split_screen.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root1 = Tk()
+
+btn1 = Button(root1,color='green')
+btn2 = Button(root1,color='blue')
+btn3 = Button(root1,color='yellow')
+
+root2 = Tk()
+root3 = Tk()
+
+root1.geometry("")
+root2.geometry("")
+root3.geometry("")
+
+root1.mainloop()
+root2.mainloop()
+root3.mainloop()
+
