f80c81b29be41e4fd150865684aad6d6df8f5b05
slamon-love <ssakirus@gmail.com>
2021-08-16 02:18:08 +0900

    fixed ploblem

--- split_screen(ThemaList).py
+++ split_screen(ThemaList).py
@@ -1,24 +1,26 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
-from PyQt5.QtWidgets import QListWidget
+# from PyQt5.QtWidgets import QListWidget, QHBoxLayout, QVBoxLayout
+
 
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
 form_class = uic.loadUiType("ThemaListUI.ui")[0]
 
 #화면을 띄우는데 사용되는 Class 선언
-class WindowClass(QMainWindow, form_class) :
+class WindowClass(QWidget, form_class) :
     def __init__(self) :
         super().__init__()
         # self.setupUi(self)
         self.initUI()
-        # self.setGeometry(803, 300, 257, 322)
+        self.setGeometry(803, 300, 257, 322)
+
 
     def initUI(self):
         #리스트 위젯 추가
-        # themlist = QListWidget(self)
-        # themlist.resize(251, 270)
+        themlist = QListWidget(self)
+        themlist.resize(251, 300)
         #버튼 추가
         addbtn = QPushButton('추가', self)
         addbtn.setCheckable(True)
@@ -27,25 +29,28 @@ class WindowClass(QMainWindow, form_class) :
         delbtn = QPushButton('삭제', self)
         delbtn.setCheckable(True)
         delbtn.toggle()
-        
+
+        optbtn = QPushButton('설정', self)
+        optbtn.setCheckable(True)
+        optbtn.toggle()
        
 
         #레이아웃
         hbox = QHBoxLayout()
+        hbox.addStretch(1)
         hbox.addWidget(addbtn)
         hbox.addStretch(1)
         hbox.addWidget(delbtn)
-        # hbox.addStretch(3)
-        # hbox.addWidget(optbtn)
+        hbox.addStretch(3)
+        hbox.addWidget(optbtn)
 
-        # vbox = QVBoxLayout()
-        # vbox.addWidget(themlist)
-        # vbox.addStretch(1)
-        # vbox.addLayout(hbox)
+        vbox = QVBoxLayout()
+        vbox.addWidget(themlist)
+        vbox.addLayout(hbox)
         
-        # self.setLayout(vbox)
-        self.setLayout(hbox)
-        self.setGeometry(803, 300, 257, 322)
+        
+        self.setLayout(vbox)
+        # self.setLayout(hbox)
         self.show()
        
 
